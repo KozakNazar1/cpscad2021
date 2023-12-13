ecce768712bcdeaefbe24cfe17dfc329c76358d9
KozakNazar1 <33431908+KozakNazar1@users.noreply.github.com>
2023-12-13 07:44:45 +0200

    Add files via upload

--- app_ct10b/src/main/java/org/eom/cscad/ct10/dao/PersonDAOImpl.java
+++ app_ct10b/src/main/java/org/eom/cscad/ct10/dao/PersonDAOImpl.java
@@ -6,8 +6,7 @@ import java.util.List;
 
 import javax.persistence.Query;
 
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 //import org.springframework.stereotype.Repository;
